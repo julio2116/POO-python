306b5eab71ddecc3466cb3d997415e5e3acb2205
Julio2116 <juliobatista2116@gmail.com>
2025-11-17 12:23:43 -0300

    Final implementation

--- atividade-05/cliente.py
+++ atividade-05/cliente.py
@@ -1,20 +1,24 @@
 import socket
 
-server_address = ('127.0.0.1', 5000)
+def clientRequest(msg):
+    server_address = (socket.gethostname(), 5000)                   #Trocar o host pela função .gethostname() retorna o Ip de rede local
 
-MSG = input("Informe a mesagem:\n")
+    MSG = msg
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)           #Ao usar socket.SOCKET_STREAM, por padrão usamos o protocolo TCP
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)           #Ao usar socket.SOCKET_STREAM, por padrão usamos o protocolo TCP
 
-s.connect(server_address)                                       #Estabelece a conxão com o servidor
-                                                                #.connect() espera receber uma tupla com com o IP e a porta
-s.sendall(str.encode(MSG))                                      #Envia informações a conexão
+    s.connect(server_address)                                       #Estabelece a conxão com o servidor
+                                                                    #.connect() espera receber uma tupla com com o IP e a porta
+    s.sendall(str.encode(MSG))                                      #Envia informações a conexão
 
-data = s.recv(1024)
+    data = s.recv(1024)
 
-print(data)
+    print(data)
 
-s.close()
+    s.close()
+
+msg = input("Informe a mesagem:\n")
+clientRequest(msg)
 
 #TCP é um protocolo confiável que garante a transmissão de toda informação,
 #pois considerando as limitações de internet, e processamento de cada dispositivo,
--- atividade-05/servidor.py
+++ atividade-05/servidor.py
@@ -1,21 +1,24 @@
 import socket
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+def server():
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-server_address = ('localhost', 5000)
+    server_address = ('', 5000)                             #host vazio sinaliza que o servidor aceitra conexões de qualquer fonte interna, mas obrigatoriamente o computador precisa saber qual porta "escuta" as requisições, é equivalente a 0.0.0.0 para IPv4 e :: para IPv6, 127.0.0.1 e localhost aceitariam apenas conexões internas
 
-s.bind(server_address)                                  #.bind() espera receber uma tupla com com o IP e a porta, por conta de socket.AF_INET, passado como primeiro argumento em socket.socket() (IPv4)
-s.listen(1)                                             #Número de conexões em fila esperando ser processadas, após alcançar o limite as novas conexões serão recusadas, esta função apenas espera uma conexão e então chama .accept()
+    s.bind(server_address)                                  #.bind() espera receber uma tupla com com o IP e a porta, por conta de socket.AF_INET, passado como primeiro argumento em socket.socket() (IPv4)
+    s.listen(1)                                             #Número de conexões em fila esperando ser processadas, após alcançar o limite as novas conexões serão recusadas, esta função apenas espera uma conexão e então chama .accept()
 
-connection, address = s.accept()                        #Retorna um novo objeto do tipo socket, uma nova conexão diferente da de .listen(), para envio e recebimento de dados
+    connection, address = s.accept()                        #Retorna um novo objeto do tipo socket, uma nova conexão diferente da de .listen(), para envio e recebimento de dados
+    print(address)
+    data = connection.recv(1024)                            #Variavel conn, nunca havia sido declarada, recebe informações do cliente, 1024 é a quantidade máxima de dados a ser recebida de cada vez
 
-data = connection.recv(1024)                            #Variavel conn, nunca havia sido declarada, recebe informações do cliente
+    if not data:
+        connection.sendall(data)                            #Usar .send() faria com que tivessemos que checar se todos os dados foram enviados e então chamar .send() quantas vezes forem necessárias
+    print(data)
+    MSG = data.decode().upper()
+    connection.sendall(str.encode(MSG))
 
-if not data:
-    connection.sendall(data)
-print(data)
-MSG = data.decode().upper()
-connection.sendall(str.encode(MSG))
+    connection.close()                                      #Neste ponto existem dois sockets abertos, por isso ambos devem ser fechados, s sendo o servidor ouvindo requisições e connection sendo a conexão de transmissão de dados, aberta sempre que uma requisição é recebida
+    s.close()                                               #O garbage colector do python realiza o close() automaticamente se for necessário, porém é uma má prática não fazer isso de forma esplicita
 
-connection.close()
-s.close()                                               #O garbage colector do python realiza o close() automaticamente se for necessário, porém é uma má prática não fazer isso de forma esplicita
+server()
