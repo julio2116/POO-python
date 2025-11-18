cd6cb1e2123a5419134aa01df018baad42f06abb
Julio2116 <juliobatista2116@gmail.com>
2025-11-18 13:07:01 -0300

    Adding new classes implementation

--- atividade-07/main.py
+++ atividade-07/main.py
@@ -2,8 +2,111 @@
 # Implemente aqui as classes solicitadas no enunciado
 # Use os mesmos nomes de classes e métodos.
 
+class Pessoa:
+    def __init__(self, nome: str = None):
+        self.nome = nome
+
+    @property
+    def nome(self):
+        return self.__nome
+    
+    @nome.setter
+    def nome(self, nome:str = None):
+        if nome is None or not isinstance(nome, str) or len(nome) < 1 or nome.isspace():
+            raise ValueError("Informe um valor válido, apenas strings não vazias")
+        self.__nome = nome
+
+    def apresentar(self):
+        print(f"Olá, eu sou {self.__nome}")
+
+class Funcionario(Pessoa):
+    def __init__(self, nome: str = None, salario: float|int = None):
+        super().__init__(nome)
+        self.salario = salario
+
+    @property
+    def salario(self):
+        return self.__salario
+
+    @salario.setter
+    def salario(self, salario: float|int = None):
+        if salario is None or isinstance(salario, (float, int)) or salario <= 0:
+            raise ValueError("Atributo salario deve ser do tipo int ou float e ter valor acima de 0")
+        self.__salario = float(salario)
+
+    def calcular_salario(self):
+        return self.salario
+    
+    def apresentar(self):
+        super().apresentar()
+        print("Atualmente estou trabalhando")
 
 
+class Estudante(Pessoa):
+    def __init__(self, nome:str = None, curso:str = None, bolsa: float|int = None):
+        super().__init__(nome)
+        self.curso = curso
+        self.bolsa = bolsa
+
+    @property
+    def curso(self):
+        return self.__curso
+    
+    @property
+    def bolsa(self):
+        return self.__bolsa
+
+    @curso.setter
+    def curso(self, curso:str = None):
+        if curso is None or not isinstance(curso, str) or len(curso) < 1 or curso.isspace():
+            raise ValueError("Curso não pode estar vazio")
+        self.__curso = curso
+    
+    @bolsa.setter
+    def bolsa(self, bolsa: float|int = None):
+        if bolsa is not None and (not isinstance(bolsa, (int, float)) or len(bolsa) < 1):
+            raise("Bolsa deve ser um numero acima de 0")
+        self.__bolsa = bolsa
+
+    def calcular_bolsa(self):
+        if self.__bolsa is not None:
+            return self.__bolsa
+        return f"O Aluno {self.nome} não possui bolsa"
+    
+    def apresentar(self):
+        print(f"Meu nome é {self.nome} e sou estudante")
+
+
+class Gerente(Funcionario):
+    def __init__(self, nome, salario, equipe:list[Funcionario] = None):
+        super().__init__(nome, salario)
+        self.equipe = equipe
+
+    @property
+    def equipe(self):
+        return self.__equipe
+    
+    @equipe.setter
+    def equipe(self, equipe:list[Funcionario] = None):
+        if equipe is not None and not isinstance(equipe, list):
+            raise ValueError("Atributo equipe deve ser do tipo list de Funcionarios ou do tipo Funcionario")
+        if equipe is not None and isinstance(equipe, list):
+            for funcionario in equipe:
+                if not isinstance(funcionario, Funcionario):
+                    raise ValueError("Todos os funcionários devem ser do tipo Funcionario")
+            self.__equipe = equipe
+        
+    def incluirFuncionario(self, funcionario: str = None):
+        if funcionario is not None and isinstance(funcionario, Funcionario):
+            self.__equipe.append(funcionario)
+
+    def apresentar(self):
+        return f"Olá sou o gerente da equipe"
+    
+
+class AssistentePesquisa(Estudante, Funcionario):
+    def __init__(self, nome, curso, salario, bolsa):
+        super().__init__(nome, curso, salario, bolsa)
 
 
 # ======== ÁREA DO PROFESSOR — NÃO ALTERAR ========
