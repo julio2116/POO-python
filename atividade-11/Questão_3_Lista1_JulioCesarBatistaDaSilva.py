ddd6efc56cfa60a590a32586bae85d3186058380
Julio2116 <juliobatista2116@gmail.com>
2025-12-03 11:12:40 -0300

    Adding third question resolution 11 activity

--- atividade-11/Questão_3_Lista1_JulioCesarBatistaDaSilva.py
+++ atividade-11/Questão_3_Lista1_JulioCesarBatistaDaSilva.py
@@ -2,3 +2,50 @@
 #Fundamentos de Estrutura de Dados e Estruturas Lineares - 2° semestre
 #Júlio Cesar Batista da Silva
 
+#3. Uma turma de formandos está vendendo rifas para angariar recursos financeiros para sua cerimônia de formatura.
+#Construa um programa para cadastrar os nomes das pessoas que compraram a rifa.
+#Ao fim, o programa deve sortear o ganhador do prêmio e imprimir o seu nome.
+import random
+
+class Rifa:
+    def __init__(self, nomes: str | list[str]):
+        self.nomes = nomes
+
+    @property
+    def nomes(self):
+        return self._nomes
+    
+    @nomes.setter
+    def nomes(self, valor = None):
+        if(valor is None or not isinstance(valor, (str, list))):
+            raise ValueError("Argumento precisa ser uma lista ou uma string")
+        if(isinstance(valor, str)):
+            self._nomes = list(valor)
+            return
+        self._nomes = valor
+
+    def cadastrarNovo(self, valor = None):
+        if(valor is None or not isinstance(valor, (str, list))):
+            raise ValueError("Argumento precisa ser uma lista ou uma string")
+        if(isinstance(valor, list)):
+            for i in valor:
+                self._nomes.append(i)
+            return
+        self._nomes.append(valor)
+
+    def sortear(self):
+        aleatorio = random.randint(0, len(self._nomes) - 1)
+        return self._nomes[aleatorio]
+    
+    def __str__(self):
+        string = ""
+        for i in self._nomes:
+            string += f"{i}"
+        return string
+
+sorteio = Rifa(["Julio", "João"])
+
+sorteio.cadastrarNovo("Pedro")
+sorteio.cadastrarNovo(["João", "Maria"])
+
+print(sorteio.sortear())
